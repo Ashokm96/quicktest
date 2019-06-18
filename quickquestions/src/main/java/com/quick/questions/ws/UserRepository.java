93c6019f16863269df6eb8dff76e5b1221f776e5
rajeshbattula631 <rajesh.battula631@gmail.com>
2019-06-18 19:58:43 +0530

    unique removed.

--- quickquestions/src/main/java/com/quick/questions/ws/UserRepository.java
+++ quickquestions/src/main/java/com/quick/questions/ws/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.quick.questions.ws.io.entity.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
+	UserEntity findByEmail(String email);
 }
