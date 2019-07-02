3672c4ee05154e91b017ca1b640a77fc277ea370
rajeshbattula631 <rajesh.battula631@gmail.com>
2019-07-02 23:11:54 +0530

    Authorization and session management added

---
+++ quickquestions/src/main/java/com/quick/questions/ws/security/AuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.quick.questions.ws.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+		
+	public AuthorizationFilter(AuthenticationManager authenticationManager) {
+		// TODO Auto-generated constructor stub
+		super(authenticationManager);
+	}
+	@Override
+	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException{
+		
+		String header =req.getHeader(SecurityConstants.HEADER_STRING);
+		if(header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+			chain.doFilter(req, resp);
+			return;
+		}
+		
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(req, resp);
+	}
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
+		// TODO Auto-generated method stub
+		String token = req.getHeader(SecurityConstants.HEADER_STRING);
+		if(token != null) {
+			token = token.replaceAll(SecurityConstants.TOKEN_PREFIX, "");
+			
+			String user = Jwts.parser()
+			.setSigningKey(SecurityConstants.TOKEN_SECRET)
+			.parseClaimsJws(token)
+			.getBody()
+			.getSubject();
+			if(user != null) {
+				return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+			}
+			return null;
+		}
+		
+		return null;
+	}
+
+}
